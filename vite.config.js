7c6d8219284c94df26726acab4655767d455423f
TONG VAN DUNG <tongvandung@gmail.com>
2026-04-28 14:08:58 +0700

    allow render host

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [".onrender.com"],
     port: 5173,
     strictPort: true
   },
   preview: {
+    allowedHosts: [".onrender.com"],
     port: 4173,
     strictPort: true
   }
